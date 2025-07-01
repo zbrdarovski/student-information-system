fa909064adbbf1b2812c3e4100759fc739cf467c
Zdravko Brdarovski <zbrdarovski@gmail.com>
2025-07-01 17:06:22 +0200

    feat(app): add AppComponent and configure routing with lazy-loaded /overview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,25 @@
 import { Routes } from '@angular/router';
-import { StudentListComponent } from './students/components/list/student-list.component';
-import { AddStudentComponent } from './students/components/add/add-student.component';
-import { EditStudentComponent } from './students/components/edit/edit-student.component';
 
 export const routes: Routes = [
-  { path: '', component: StudentListComponent },
-  { path: 'add', component: AddStudentComponent },
-  { path: 'edit/:id', component: EditStudentComponent },
-  { path: '**', redirectTo: '' }
+  {
+    path: 'overview',
+    loadComponent: () => import('./students/components/list/student-list.component').then(m => m.StudentListComponent)
+  },
+  {
+    path: 'overview/add',
+    loadComponent: () => import('./students/components/add/add-student.component').then(m => m.AddStudentComponent)
+  },
+  {
+    path: 'overview/edit/:id',
+    loadComponent: () => import('./students/components/edit/edit-student.component').then(m => m.EditStudentComponent)
+  },
+  {
+    path: '',
+    redirectTo: '/overview',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/overview'
+  }
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { StudentListComponent } from './app/students/components/list/student-list.component';
+import { AppComponent } from './app/component/app.component';
 
-bootstrapApplication(StudentListComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
